d24c2d3a5fff653bcfb5e80a9f84d862d46b9c29
gurjap <gurjaph@my.yorku.ca>
2022-08-13 13:15:52 -0400

    cors error fixed

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const bodyParser = require("body-parser")
 require("dotenv/config")
 const mongoose = require("mongoose");
 
+//fixes the cors error on the frontend
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) // Use this after the variable declaration
+
 //import ROUTES
 const LogScaleRoute = require("./routes/logScale")
 
