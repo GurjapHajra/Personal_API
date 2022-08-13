f87f1434977de4642a76e8a895a08249e5650f4f
gurjap <gurjaph@my.yorku.ca>
2022-08-13 15:32:58 -0400

    corrs bugfixing

--- app.js
+++ app.js
@@ -6,13 +6,8 @@ const mongoose = require("mongoose");
 
 //fixes the cors error on the frontend
 const cors=require("cors");
-const corsOptions ={
-   origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-}
 
-app.use(cors(corsOptions)) // Use this after the variable declaration
+app.use(cors()) // Use this after the variable declaration
 
 //import ROUTES
 const LogScaleRoute = require("./routes/logScale")
